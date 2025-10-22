09525aa17a15757424b1f9c7898de845ee42f26d
Mjsoylan <mjsoylan@hiof.no>
2025-10-22 20:54:57 +0200

    feat: added part of repostory for libary
    added repostry for libray get,creat,getbyid,deletebyid

--- src/features/bookshelf/repository.ts
+++ src/features/bookshelf/repository.ts
@@ -1,5 +1,57 @@
-/*
+import {eq} from "drizzle-orm";
+import {libraries, library} from "../../db/schema";
+import { db } from "../../db/index";
 
-Det skal være kode her som snakker med databasen og henter data hvilket blir sendt til den funksjonen i services som kaller den.
 
-*/
+
+
+export const getlibraries = async () => {
+  try {
+    const returnlibraries: library[] = await db.select().from(libraries);
+    return { success: true, data: returnlibraries }
+  } catch (error) {
+    return { success: false, error: 'Failed getting libraries' }
+  }
+}
+
+export const create = async (data : any) => {
+  try {
+    const libary = await db.insert(libraries).values({
+        userId: data.userId,
+        name: data.name,
+        text: data.text,
+        cordinats: data.cordinats,
+        books: data.books,
+        createdAt: data.createdAt,
+    }).returning();
+    return { success: true, data: libary }
+  } catch (error) {
+    return { success: false, error: 'Failed creating libary' }
+  }
+}
+
+
+export const edit = async (id: string,data : any) => {
+ try {
+    return { succes :false}
+  } catch (error) {
+    return { success: false, error: 'Failed edit' }
+  }
+}
+export const getbyid = async (id: string) => {
+  try {
+    const libary = await db.select().from(libraries).where(eq(libraries.id, id));
+    return { success: true, data: libary }
+  } catch (error) {
+    return { success: false, error: 'Failed getting libary by id' }
+  }
+}
+
+export const deletebyid = async (id: string) => {
+  try {
+    await db.delete(libraries).where(eq(libraries.id, id)); 
+    return { success: true }
+    } catch (error) {
+    return { success: false, error: 'Failed deleting libary by id' }
+    }
+}
