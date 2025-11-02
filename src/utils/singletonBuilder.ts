f269142dfed34405d656b271553ef722e0d1dd8b
ResistanceIsCharacterForming <nikol.lysebraate@hiof.no>
2025-11-02 11:41:35 +0100

    feat: Skrev om singletonMaster til å bruke lazy loading
    Nødvendig med lazy loading for singletons for å enklere jobbe med kobling opp mot databasen
    BREAKING CHANGES: singletonBuilder, library/repository, library/controller, db/index

--- src/utils/singletonBuilder.ts
+++ src/utils/singletonBuilder.ts
@@ -7,12 +7,54 @@ import { createReportService } from "@/features/report/service"
 import { createReviewController } from "@/features/review/controller"
 import { createReviewService } from "@/features/review/service"
 import { createLibraryRepository } from "@/features/library/repository"
+import { createDbConnection } from "@/db/index"
 
-export const singletonMaster = ({
-    libraryController: createLibraryController(createLibraryService( createLibraryRepository() )),
-    userController: createUserController(createUserService()),
-    reportController: createReportController(createReportService()),
-    reviewController: createReviewController(createReviewService())
+/* https://www.typescriptlang.org/docs/handbook/basic-types.html */
 
+/* Lazy loader for alle singletons. */
+export const singletonMaster = {
+    /* Privat property. Set som null, altså blank når objektet lages. Bruk ReturnType for å matche typen til hva createDbConnection gir oss. */
+    _dbConnection: null as ReturnType<typeof createDbConnection> | null,
+    /* Getter. Objektet blir "kjøprt" på denne måten: singletonMaster.dbConnection */
+    get dbConnection() {
+        /* Er den private property "initaiswert" */
+        if (!this._dbConnection) {
+            /* Lag ny singleton */
+            this._dbConnection = createDbConnection()
+        }
+        /* Send tilbake privat property */
+        return this._dbConnection
+    },
 
-})
+    _libraryController: null as ReturnType<typeof createLibraryController> | null,
+    get libraryController() {
+        if (!this._libraryController) {
+            this._libraryController = createLibraryController(createLibraryService( createLibraryRepository() ))
+        }
+        return this._libraryController
+    },
+
+    _userController: null as ReturnType<typeof createUserController> | null,
+    get userController() {
+        if (!this._userController) {
+            this._userController = createUserController(createUserService())
+        }
+        return this._userController
+    },
+
+    _reportController: null as ReturnType<typeof createReportController> | null,
+    get reportController() {
+        if (!this._reportController) {
+            this._reportController = createReportController(createReportService())
+        }
+        return this._reportController
+    },
+
+    _reviewController: null as ReturnType<typeof createReviewController> | null,
+    get reviewController() {
+        if (!this._reviewController) {
+            this._reviewController = createReviewController(createReviewService())
+        }
+        return this._reviewController
+    }
+}
