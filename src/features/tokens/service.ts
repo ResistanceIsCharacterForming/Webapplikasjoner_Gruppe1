ea2d90448d7388eef53ac8ec00da58f82d699fc1
ResistanceIsCharacterForming <nikol.lysebraate@hiof.no>
2025-11-11 16:58:32 +0100

    fix: Tokens sin service tar nå høyde for at det kan finnes flere enn en cookie

--- src/features/tokens/service.ts
+++ src/features/tokens/service.ts
@@ -9,8 +9,19 @@ export function createTokensService() {
         async checkCredentials(ctx: any) {
 
             let cookie: string | undefined = ctx.request.headers.get("cookie") ?? undefined
-            
+
             if (cookie === undefined) return false
+            
+            if (cookie.includes(";")) {
+                
+                cookie = ctx.request.headers.get("cookie").split(";")
+
+                const singleCookie = cookie.filter(x => x.includes("jwtToken"))
+                
+                cookie = singleCookie[0]
+            }
+
+            if (!cookie.includes(":")) return false
 
             let jwt: string = cookie.split(":")[1]
 
