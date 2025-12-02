c9d6c75ef20d4df17dcf9271f4dca2420ea7975c
ResistanceIsCharacterForming <nikol.lysebraate@hiof.no>
2025-12-02 15:16:29 +0100

    feat: NAV-bar v1

--- src/middleware/authHandler.ts
+++ src/middleware/authHandler.ts
@@ -49,7 +49,8 @@ export async function authCheck ({
         "/home",
         "/api/v1/tokens",
         "/api/v1/users",
-        "/api/v1/libraries"
+        "/api/v1/libraries",
+        "/api/v1/image"
     ]
 
     const isOpenRoute = (openRoutes.includes(requestedPathname))
